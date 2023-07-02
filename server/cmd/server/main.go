22b318bdd46ae28ea5feaa6f12ac0a086a9dbfe7
kyosu-1 <sho013039@gmail.com>
2023-07-02 19:42:59 +0900

    impl main

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -1,22 +1,26 @@
 package main
 
 import (
-	"context"
-	"os"
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/kyosu-1/reversi/server/internal/game"
+	"github.com/kyosu-1/reversi/server/internal/server"
+	"github.com/kyosu-1/reversi/server/internal/strategy"
 )
 
-const (
-	ExitOK = iota
-	ExitError
-)
+var addr = flag.String("addr", "localhost:8080", "http service address")
 
 func main() {
-	if err := run(context.Background()); err != nil {
-		os.Exit(ExitError)
-	}
-	os.Exit(ExitOK)
-}
+	flag.Parse()
+	log.SetFlags(0)
 
-func run(ctx context.Context) error {
-	return nil
+	s := server.NewServer(game.NewBoard(), &strategy.RandomStrategy{})
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		server.ServeWs(s, w, r)
+	})
+
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
